39861dec5c80b58f51a0f8c7508b58a668f0935f
Shivani Barman <shivanibarman247@gmail.com>
2026-04-29 23:13:44 +0530

    feat: add assignment module with CRUD APIs

---
+++ backend/app/models/assignment_model.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from app.db.database import Base
+from datetime import datetime
+
+class Assignment(Base):
+    __tablename__ = "assignments"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+
+    subject = Column(String, nullable=False)
+
+    total_marks = Column(Integer, nullable=True)
+    obtained_marks = Column(Integer, nullable=True)
+
+    assigned_date = Column(DateTime, default=datetime.utcnow)
+    due_date = Column(DateTime, nullable=False)
+
+    file_url = Column(String, nullable=True)
+
+    college_id = Column(Integer, ForeignKey("colleges.id"), nullable=False)
+
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    
